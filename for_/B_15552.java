0aa9dc706b5909244c7b78fdb21a25e9f9c56af7
Lux-Hyeji <foggy312@gmail.com>
2021-12-04 11:15:11 +0900

    for_

--- for_/B_15552.java
+++ for_/B_15552.java
@@ -18,17 +18,18 @@ public class B_15552 {
 
         for(int i = t; i > 0; i--){
             String s = br.readLine(); 
-            //StringTokenizer st = new StringTokenizer(s);
-            //int a = Integer.parseInt(st.nextToken());
-            //int b = Integer.parseInt(st.nextToken());
-            String[] array = s.split(" "); //공백마다 데이터 끊어서 배열에 넣음
-            bw.write(Integer.parseInt(array[0])+Integer.parseInt(array[1])+"\n");
-            //bw.write(a+b+"\n"); // 문자형으로 자동 변환 해버림; bw.write(33+44) -> M
+            StringTokenizer st = new StringTokenizer(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            //String[] array = s.split(" "); //공백마다 데이터 끊어서 배열에 넣음
+            //bw.write(Integer.parseInt(array[0])+Integer.parseInt(array[1])+"\n");
+            bw.write(a+b+"\n"); // 문자형으로 자동 변환 해버림; bw.write(33+44) -> M
             //bw.newline() // 개행
+            //bw.flush();  // 없어도 왜 되는거냐
         }
 
         br.close();
-        bw.flush(); // 여기서 버퍼에 있는거 다 출력 write만난다고 출력되는거 아님
+        //bw.flush(); // 여기서 버퍼에 있는거 다 출력 write만난다고 출력되는거 아님
         bw.close();
 
     }
